e9b7aafc2517236771b99b205c6a2124367223bb
Nicholas Phillips <Nicophillips@live.com>
2021-03-08 19:11:31 -0500

    Update metatags.io.user.js

--- scripts_other/metatags.io.user.js
+++ scripts_other/metatags.io.user.js
@@ -7,7 +7,7 @@
 // @downloadURL https://github.com/LagSwitchedVirginity/UserScripts-and-UserStyles/raw/master/scripts_combos/metatags.io.user.js
 // @name Title Function Metatags.io
 // @description Adds a function to the JS Console named updateDomain(string) that allows you to change the preview domain.
-// @version 1615247925153
+// @version 1615248688512
 // @match *://metatags.io/*
 // @run-at document-end
 // ==/UserScript==
@@ -27,15 +27,16 @@ var updateDomain = function (dom) {
 };
 
 function addCustomDomainInput() {
-    document.querySelector(".nav-search").innerHTML += "<input class=\"\" type=\"text\" placeholder=\"custom domain\" style=\"margin-left: 15px;\">";
+    var searchNav = document.querySelector(".nav-search");
     var htmlInputElement = document.createElement("input");
     htmlInputElement.className = "nav-search__input c_domain";
     htmlInputElement.type = "text";
     htmlInputElement.placeholder = "custom domain";
     htmlInputElement.style.marginLeft = "15px";
-    htmlInputElement.addEventListener("change",function () {
+    htmlInputElement.addEventListener("keyup",function () {
         updateDomain(this.value);
-    })
+    });
+    searchNav.appendChild(htmlInputElement);
 }
 
 addCustomDomainInput();
